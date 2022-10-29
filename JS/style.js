513cfaa9fdb5ec46682058bd23e0b63d5664ff39
ahmed boulaouane <bouahmed530@gmail.com>
2022-10-29 09:56:56 +0100

    initialisation

--- JS/style.js
+++
@@ -1,7 +0,0 @@
-const main_Video = document.getElementsByClassName('main_Vid')[0];
-
-window.addEventListener('scroll',()=>{
-    let Y = window.scrollY;
-    let opacity = 60-(Y/7) > 0 ? 60-(Y/7) : 0;
-    main_Video.style.opacity = `${opacity}%`;
-});
